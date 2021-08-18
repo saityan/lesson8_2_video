f58b824c34aafabc141c34938efaf096c1b8b8e0
saityan <naminatosky@gmail.com>
2021-08-19 00:53:59 +0300

    added parcelable for CardData class

--- app/src/main/java/ru/geekbrains/lesson8_2_video/model/domain/CardData.java
+++ app/src/main/java/ru/geekbrains/lesson8_2_video/model/domain/CardData.java
@@ -1,6 +1,9 @@
 package ru.geekbrains.lesson8_2_video.model.domain;
 
-public class CardData {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class CardData implements Parcelable {
     private String title;
     private String description;
     private int picture;
@@ -13,6 +16,38 @@ public class CardData {
         this.picture = picture;
         this.like = like;
     }
+
+    protected CardData(Parcel in) {
+        title = in.readString();
+        description = in.readString();
+        picture = in.readInt();
+        like = in.readByte() != 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeString(description);
+        dest.writeInt(picture);
+        dest.writeByte((byte) (like ? 1 : 0));
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<CardData> CREATOR = new Creator<CardData>() {
+        @Override
+        public CardData createFromParcel(Parcel in) {
+            return new CardData(in);
+        }
+
+        @Override
+        public CardData[] newArray(int size) {
+            return new CardData[size];
+        }
+    };
 
     public String getTitle() {
         return title;
